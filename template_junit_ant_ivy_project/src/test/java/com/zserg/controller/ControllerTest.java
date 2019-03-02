8de7aa60ebea9f5c38071770da89acc1fddb0094
Sergey Zadyabin <zsergt3@gmail.com>
2019-03-02 23:36:58 +0300

    add lombok and logging

--- template_junit_ant_ivy_project/src/test/java/com/zserg/controller/ControllerTest.java
+++ template_junit_ant_ivy_project/src/test/java/com/zserg/controller/ControllerTest.java
@@ -2,16 +2,19 @@ package com.zserg.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.zserg.controller.Controller;
 
+@Slf4j
 public class ControllerTest {
 
 	@Test
 	@DisplayName("Controller Test")
 	public void test_welcome_message() {
+		log.info("Controller test started");
 		Controller obj = new Controller();
 		assertEquals("Hello!", obj.hello());
 	}
